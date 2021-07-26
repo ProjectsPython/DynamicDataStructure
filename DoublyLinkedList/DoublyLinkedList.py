22d2fa189cb5ef7db8a84627f8f89de2b7e23ebd
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-07-26 10:06:04 -0500

    Print doubly linked list

--- DoublyLinkedList/DoublyLinkedList.py
+++ DoublyLinkedList/DoublyLinkedList.py
@@ -8,3 +8,11 @@ class DoublyLinkedList:
         self._head = None
         self._queue = None
         self._size = 0
+    def __str__(self):
+        _array = []
+        _present_node = self._head
+        while _present_node != None:
+            _array.append(_present_node._value)
+            _present_node = _present_node._next_node
+        return str(_array)+" Size: "+ str(self._size)
+    
