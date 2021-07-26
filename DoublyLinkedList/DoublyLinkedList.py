18699092194eca871da4768c3cb96fe704f93ea0
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-07-26 13:24:00 -0500

    Insert items into the doubly linked list (append)

--- DoublyLinkedList/DoublyLinkedList.py
+++ DoublyLinkedList/DoublyLinkedList.py
@@ -15,7 +15,7 @@ class DoublyLinkedList:
             _array.append(_present_node._value)
             _present_node = _present_node._next_node
         return str(_array)+" Size: "+ str(self._size)
-    def  prepend(self,_value):
+    def prepend(self,_value):
         _new_node = self._Node(_value)
         if self._head == None and self._queue == None:
             self._head = _new_node
@@ -25,10 +25,22 @@ class DoublyLinkedList:
             _new_node._next_node = self._head
             self._head = _new_node
         self._size += 1
+    def append(self,_value):
+        _new_node = self._Node(_value)
+        if self._head == None and self._queue == None:
+            self._head = _new_node
+            self._queue = _new_node
+        else:
+            self._queue._next_node = _new_node
+            _new_node._previous_node = self._queue
+            self._queue = _new_node
+        self._size += 1
+
+
 
 obj_node = DoublyLinkedList()
-obj_node.prepend(1)
-obj_node.prepend(2)
-obj_node.prepend(3)
-obj_node.prepend(4)
+obj_node.append(1)
+obj_node.append(2)
+obj_node.append(3)
+obj_node.append(4)
 print(obj_node)
