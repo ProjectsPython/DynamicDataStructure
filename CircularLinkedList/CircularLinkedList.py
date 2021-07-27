e106182042a5e3972b29a7b16fe1da961574cd06
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-07-27 12:04:24 -0500

    Creating the circular linked list class

---
+++ CircularLinkedList/CircularLinkedList.py
@@ -0,0 +1,9 @@
+class CircularLinkedList:
+    class _Node:
+        def __init__(self,_value):
+            self._value =  _value
+            self._next_node = None
+    def __init__(self):
+        self._head = None
+        self._queue = None
+        self._size = 0
---
+++ CircularLinkedList/README.md
@@ -0,0 +1,1 @@
+## Data Structure: Circular Linked List
