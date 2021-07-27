e1831bc0304d81d4722ed0357ff3b6390bdb300c
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-07-27 13:18:38 -0500

    Print the circular linked list

--- CircularLinkedList/CircularLinkedList.py
+++ CircularLinkedList/CircularLinkedList.py
@@ -7,3 +7,17 @@ class CircularLinkedList:
         self._head = None
         self._queue = None
         self._size = 0
+    def __str__(self):
+        _array = []
+        _pivot = True
+        _present_node = self._head
+        _accountant = self._size
+        while _accountant != 0:
+            if _pivot != False or _present_node != self._head:
+                _array.append(_present_node._value)
+                _present_node = _present_node._next_node
+                _pivot = False
+                _accountant -= 1
+            else:
+                 break
+        return str(_array) + " Size: " + str(self._size)                
