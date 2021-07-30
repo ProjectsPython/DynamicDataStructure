50e20c5910fdb2e2020ff2e63458f1756fdea1c5
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-07-30 18:27:28 -0500

    Detail solution in doubly linked list

--- DoublyLinkedList/DoublyLinkedList.py
+++ DoublyLinkedList/DoublyLinkedList.py
@@ -4,10 +4,12 @@ class DoublyLinkedList:
             self._value =  _value
             self._previous_node = None
             self._next_node = None
+
     def __init__(self):
         self._head = None
         self._queue = None
         self._size = 0
+
     def __str__(self):
         _array = []
         _present_node = self._head
@@ -15,6 +17,7 @@ class DoublyLinkedList:
             _array.append(_present_node._value)
             _present_node = _present_node._next_node
         return str(_array)+" Size: "+ str(self._size)
+
     def prepend(self,_value):
         _new_node = self._Node(_value)
         if self._head == None and self._queue == None:
@@ -25,6 +28,7 @@ class DoublyLinkedList:
             _new_node._next_node = self._head
             self._head = _new_node
         self._size += 1
+
     def append(self,_value):
         _new_node = self._Node(_value)
         if self._head == None and self._queue == None:
@@ -44,8 +48,9 @@ class DoublyLinkedList:
             _removed_node = self._head
             self._head = _removed_node._next_node
             _removed_node._next_node = None
-            self._size -=1
+            self._size -= 1
         return _removed_node._value
+
     def pop(self):
         if self._size == 0:
             self._head = None
@@ -55,8 +60,9 @@ class DoublyLinkedList:
             self._queue = _removed_node._previous_node
             self._queue._next_node = None
             _removed_node._next_node = None
-            self._size -=1
+            self._size -= 1
             return _removed_node._value
+
     def get(self, _index):
         if _index == self._size - 1:
             return self._queue
@@ -75,19 +81,21 @@ class DoublyLinkedList:
                 _present_node = self._queue
                 _accountant = self._size - 1
                 while _accountant != _index:
-                    _present_node = _present_node._next_node
+                    _present_node = _present_node._previous_node
                     _accountant -= 1
                 return _present_node
         else:
             return None
+
     def update(self,_index, _value):
         _target_node = self.get(_index)
         if _target_node != None:
             _target_node._value =  _value
         else:
             return None
+
     def insert(self,_index, _value):
-        if _index == self._size-1:
+        if _index == self._size - 1:
             return self.append(_value)
         elif _index >= 0 and _index < self._size - 1:
             _new_node = self._Node(_value)
@@ -100,8 +108,9 @@ class DoublyLinkedList:
             self._size += 1
         else:
             return None
+
     def remove(self,_index):
-        if _index == self._size-1:
+        if _index == self._size - 1:
             return self.pop()
         elif _index == 0:
             return self.shift()
@@ -117,6 +126,7 @@ class DoublyLinkedList:
             return _removed_node
         else:
             return None
+
     def reverse(self):
         _reverted_nodes = None
         _present_node = self._head
