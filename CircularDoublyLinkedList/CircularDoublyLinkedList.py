634c94a01e487498d6cd850d3f11a4f990472d4f
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-07-30 13:54:51 -0500

    Create circular doubly linked list class

---
+++ CircularDoublyLinkedList/CircularDoublyLinkedList.py
@@ -0,0 +1,17 @@
+class CircularDoublyLinkedList:
+    class _Node:
+        def __init__(self,_value):
+            self._value =  _value
+            self._previous_node = None
+            self._next_node = None
+
+    def __init__(self):
+        self._head = None
+        self._queue = None
+        self._size = 0
+
+
+        
+
+
+
---
+++ CircularDoublyLinkedList/README.md
@@ -0,0 +1,1 @@
+## Data Structure: Circular Doubly Linked List
