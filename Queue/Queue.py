4998f135a1373318329b38641c03c64c023d2539
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-08-02 10:02:29 -0500

    Creating the queue class

---
+++ Queue/Queue.py
@@ -0,0 +1,15 @@
+class Queue:
+    class _Node:
+        def __init__(self,_value):
+            self._value = _value
+            self._next_node = None
+
+    def __init__(self):
+        self._head = None
+        self._queue = None
+        self._size = 0
+        
+    
+
+
+        
