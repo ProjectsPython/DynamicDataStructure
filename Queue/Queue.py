13db6804bb586b1cf3f68e24331f8a394ddde9f5
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-08-02 10:17:21 -0500

    Print the queue

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -9,6 +9,14 @@ class Queue:
         self._queue = None
         self._size = 0
     
+    def __str__(self):
+        _array = []
+        _present_node = self._head
+        while _present_node != None:
+            _array.append(_present_node._value)
+            _present_node = _present_node._next_node
+        return str(_array) + " Size: " + str(self._size)
+    
     def enqueue(self, _value):
         _new_node = self._Node(_value)
         if self._head == None and self._queue == None:
