c19f60cfd17c7629e3d399a1cf9f5ba15ff1cbf0
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-08-02 10:04:24 -0500

    Creating enqueue method

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -8,7 +8,16 @@ class Queue:
         self._head = None
         self._queue = None
         self._size = 0
-        
+    
+    def enqueue(self, _value):
+        _new_node = self._Node(_value)
+        if self._head == None and self._queue == None:
+            self._head = _new_node
+            self._queue = _new_node
+        else:
+            self._queue._next_node = _new_node
+            self._queue = _new_node
+        self._size += 1
     
 
 
