5e63c687cae03bf0c4ea0ae3235b1751668a75db
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-08-06 14:55:10 -0500

    Implementing a Deque

---
+++ Deque/Deque.py
@@ -0,0 +1,79 @@
+class Deque:
+    class _Node:
+        def __init__(self,_value):
+            self._value = _value
+            self._next_node = None
+
+    def __init__(self):
+        self._head = None
+        self._queue = None
+        self._size = 0
+    
+    def __str__(self):
+        _array = []
+        _present_node = self._head
+        while _present_node != None:
+            _array.append(_present_node._value)
+            _present_node = _present_node._next_node
+        return str(_array) + " Size: " + str(self._size)
+    
+    def add_front(self, _value):
+        _new_node = self._Node(_value)
+        if self._head == None and self._queue == None:
+            self._head = _new_node
+            self._queue = _new_node
+        else:
+            self._queue._next_node = _new_node
+            self._queue = _new_node
+        self._size += 1
+
+    def add_rear(self, _value):
+        _new_node = self._Node(_value)
+        if self._head == None and self._queue == None:
+            self._head = _new_node
+            self._queue = _new_node
+        else:
+            _new_node._next_node = self._head
+            self._head = _new_node
+        self._size += 1
+    
+    def remove_front(self):
+        if self._size == 0:
+            self._head = None
+            self._queue = None
+        else:
+            _present_node = self._head
+            while _present_node._next_node != None:
+                _previous_node = _present_node
+                _present_node = _present_node._next_node
+            self._queue = _previous_node
+            self._queue._next_node = None
+            self._size -= 1
+            return _present_node
+
+    def remove_rear(self):
+        if self._size == 0:
+            self._head = None
+            self._queue = None
+        else:
+            _removed_node = self. _head
+            self._head = _removed_node._next_node
+            _removed_node._next_node = None
+            self._size -= 1
+            return _removed_node
+
+obj_node = Deque()
+obj_node.add_front(1)
+obj_node.add_front(2)
+obj_node.add_front(3)
+obj_node.add_front(4)
+print(obj_node)
+obj_node.add_rear(0)
+print(obj_node)
+obj_node.remove_rear()
+print(obj_node)
+obj_node.remove_front()
+print(obj_node)
+
+
+        
